89fce4703520fc79f0b892c914e90ff189d3591d
김근우 <slalek152@naver.com>
2021-06-23 17:26:12 +0900

    algorithm practice 0623 - bfs

---
+++ algorithm/src/algorithm/Problem_2178.java
@@ -0,0 +1,56 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Problem_2178 {
+	
+	static byte[] dx = {0,1,0,-1}; 
+	static byte[] dy = {1,0,-1,0};
+	static boolean[][] visited;
+	static int[][] board;
+	static int n,m;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		board = new int[n][m];
+		visited = new boolean[n][m];
+		for(int i =0;i<n;i++) {
+			String str = br.readLine();
+			for(int j =0 ;j<m;j++) {
+				board[i][j] = Integer.parseInt(str.substring(j, j+1));
+			}
+		}	
+		System.out.println(bfs());
+	}
+	
+	public static int bfs() {
+		Queue<byte[]> qu = new LinkedList<byte[]>();
+		qu.add(new byte[]{0,0,1});
+		byte[] now;
+		while(!qu.isEmpty()) {
+			byte [] next = new byte [2]; 
+			now = qu.poll();
+			visited[now[0]][now[1]] = true;
+			for(int i =0;i<4;i++) {
+				next[0] = (byte) (now[0]+dx[i]);
+				next[1] = (byte)(now[1]+dy[i]);
+				if(next[0]<0||next[0]>=n||next[1]<0||next[1]>=m||board[next[0]][next[1]]==0||visited[next[0]][next[1]])
+					continue;
+				else {
+					if(next[0]==n-1&&next[1]==m-1)
+						return now[2]+1;
+					qu.add(new byte[] {next[0],next[1],(byte)(now[2]+1)});
+				}
+			}
+		}
+		return 0;
+	}
+}
