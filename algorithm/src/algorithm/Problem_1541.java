4ca25761df9e88b7ed9000a6d6384b32d29f590a
김근우 <slalek152@naver.com>
2021-06-28 22:54:37 +0900

    algorithm practice greedy

---
+++ algorithm/src/algorithm/Problem_1541.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Problem_1541 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String num = "";
+		int ans = 0;
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int i=0;
+		int j=50;
+		for(i =0;i<str.length();i++) {
+			if(str.charAt(i)=='+'||str.charAt(i)=='-') {
+				list.add(Integer.parseInt(num));
+				num = "";
+				if(str.charAt(i)=='-') {
+					j=Math.min(list.size(), j);
+				}
+			}
+			else {
+				num += str.charAt(i);
+			}
+			
+		}
+		list.add(Integer.parseInt(num));
+		for(int k =0;k<j;k++)
+			ans += list.get(k);
+		for(int k = j;k<list.size();k++)
+			ans -= list.get(k);
+		System.out.println(ans);
+	}
+}
